aa8423a83f7dc9007436c2eb454d1fcd10e347c9
Sergio Zegarra <s.zegarra@hotmail.com>
2023-12-27 05:20:49 -0500

    Primer commit, probando el flujo de preguntas y respuestas

---
+++ app.js
@@ -0,0 +1,104 @@
+const { createBot, createProvider, createFlow, addKeyword } = require('@bot-whatsapp/bot')
+
+const QRPortalWeb = require('@bot-whatsapp/portal')
+const BaileysProvider = require('@bot-whatsapp/provider/baileys')
+const MockAdapter = require('@bot-whatsapp/database/mock')
+
+const flowSecundario = addKeyword(['2', 'siguiente']).addAnswer(['📄 Aquí tenemos el flujo secundario'])
+
+const flowDocs = addKeyword(['doc', 'documentacion', 'documentación']).addAnswer(
+    [
+        '📄 Aquí encontras las documentación recuerda que puedes mejorarla',
+        'https://bot-whatsapp.netlify.app/',
+        '\n*2* Para siguiente paso.',
+    ],
+    null,
+    null,
+    [flowSecundario]
+)
+
+const flowTuto = addKeyword(['tutorial', 'tuto']).addAnswer(
+    [
+        '🙌 Aquí encontras un ejemplo rapido',
+        'https://bot-whatsapp.netlify.app/docs/example/',
+        '\n*2* Para siguiente paso.',
+    ],
+    { capture: true },
+    (ctx, { fallBack }) => {
+        if (!ctx.body.includes('@')) return fallBack()
+    },
+    [flowSecundario]
+)
+
+const flowGracias = addKeyword(['gracias', 'grac']).addAnswer(
+    [
+        '🚀 Puedes aportar tu granito de arena a este proyecto',
+        '[*opencollective*] https://opencollective.com/bot-whatsapp',
+        '[*buymeacoffee*] https://www.buymeacoffee.com/leifermendez',
+        '[*patreon*] https://www.patreon.com/leifermendez',
+        '\n*2* Para siguiente paso.',
+    ],
+    null,
+    null,
+    [flowSecundario]
+)
+
+const flowDiscord = addKeyword(['discord']).addAnswer(
+    ['🤪 Únete al discord', 'https://link.codigoencasa.com/DISCORD', '\n*2* Para siguiente paso.'],
+    null,
+    null,
+    [flowSecundario]
+)
+
+const salir = addKeyword(['salir','terminar']).addAnswer(
+    ['Session Finalizada', 'Hasta Pronto.'],
+    null,
+    null,[]
+)
+
+
+const flowPrincipal = addKeyword(['menu', ''])
+    .addAnswer('🙌 Hola bienvenido a este *Chatbot*')
+    .addAnswer(
+        [
+            'te comparto los siguientes links de interes sobre el proyecto',
+            '👉 *1* para ver la documentación',
+            '👉 *2* para ver la lista de videos',
+            '👉 *3* Aqui los tutoriales',
+            '👉 *4* unirte al discord',
+            '❌ *0* Salir',
+        ],
+        { capture: true },
+        async (ctx, { fallBack,gotoFlow,flowDynamic }) => {
+
+            switch (ctx.body) {
+                case '0':  return gotoFlow(salir); break;
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                    console.log("Estamos ok");
+                    break;
+                default:
+                    return fallBack();
+            }
+
+        },
+        [flowDocs, flowGracias, flowTuto, flowDiscord]
+    )
+
+const main = async () => {
+    const adapterDB = new MockAdapter()
+    const adapterFlow = createFlow([flowPrincipal])
+    const adapterProvider = createProvider(BaileysProvider)
+
+    createBot({
+        flow: adapterFlow,
+        provider: adapterProvider,
+        database: adapterDB,
+    })
+
+    QRPortalWeb()
+}
+
+main()
